121de0c70d7fcb53ea046c2a4e1b3901a7322029
Leander <lvink3@gmail.com>
2022-01-06 11:17:11 +0100

    Added collection whitelist

--- src/oasis.js
+++ src/oasis.js
@@ -486,6 +486,22 @@ let erc721abi = [
     }
 ];
 
+let approvedNFTs = [
+    '0xE765026Cad648785b080E78700cBF6fa1C050d7C', // CashCats
+    '0xD27CFd5b254E8D958c0c8e7f99E17A1e33652C1A', // CryptoR.AT
+    '0x88fA0495d5E9C1B178EAc1D76DF9D729e39fD8E8', // Poolside Puffers
+    '0x142d360e65d664B3074d03A1AC3fCDECFeCBC5F9', // TROPICAL.GULLS
+    '0xe017AC8A93790571AF6a93f34cE2258dC900006B', // TROPICAL.MONSTER
+    '0xa48C513189F8971736A1e4f3E786f471bf1EBfE1', // DAIQUI.DUDES
+    '0xff48aAbDDACdc8A6263A2eBC6C1A68d8c46b1bf7', // LawPunks
+    '0x9F6466C0ffe9245d994C18c8B0575Af22a5AeEd5', // Cattos
+    '0xf913c55C9E3642dbaA62c26Ff010e97565DeD3B1', // Fatcat
+    '0x8FdC63Fe8496D819731e1d447B1eB35951798AA3', // DAOer
+    '0x23d9B4b351d5C57f38206dB0697B891d2A32732E', // Potato
+    '0xdccB0e678bEA8FE3d97921CbFF85Be757a223312', // BWB series
+    '0x147f7D752ed7375E4e7B50aC2C94723F171cE90d', // RealMoutai
+];
+
 // Order type # to name
 let translateOrderType = function(type) {
     var translatedOrderType = 'Unknown';
@@ -515,6 +531,11 @@ let getPhotoForToken = async function(token, id) {
 
 // Send TG messages
 let sendTgMessage = async function(token, id, message) {
+    // Only approved collections
+    if(!approvedNFTs.includes(token)) {
+        return;
+    }
+
     getPhotoForToken(token, id).then(function(photo) {
         if(photo === null) {
             // No photo? Display OASIS logo instead
@@ -531,9 +552,10 @@ let sendTgMessage = async function(token, id, message) {
             // Send message
             // @TODO change this to a less awkward way to send the message. Use CURL directly from JS ideally
             var cmd = shellescape(['php', 'bin/tg-message.php', this.photo, fullMessage]);
-            exec(cmd, (error, stdout, stderr) => {
+            console.log(cmd);
+            //exec(cmd, (error, stdout, stderr) => {
                 // console.log(stdout);
-            });
+            //});
         }.bind({token: this.token, id: this.id, photo: photo, message: this.message}));
     }.bind({token: token, id: id, message: message}));
 }
